98eb889a36c9ab8e3a962cd171f56651c8ed6280
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-10-24 11:29:24 -0300

    CRUD1

---
+++ clase22-CRUD1/src/controllers/productsController.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {res.render("products", { productos : products})}, 
+
+	detail: (req, res) => {
+        const productId = parseInt(req.params.id,10);
+        let productFounded;
+        for (let i=0; i<=products.length; i++) {
+            if (products[i].id === productId) {
+               productFounded = products[i]
+               
+            } 
+        }
+		  return res.render("detail", {producto: productFounded});
+    },
+
+	create: (req, res) => {res.render("product-create-form")},
+	
+	store: (req, res) => {
+	
+		console.log(req.file);
+		let productoNuevo = {
+			id: products.length + 1,
+			name: req.body.name,
+			price: parseInt(req.body.price),
+			discount: parseInt(req.body.discount),
+			category: req.body.category,
+			description: req.body.description,
+			image: req.file.filename
+		}
+	
+		products.push(productoNuevo);
+		
+		productsJson = JSON.stringify(products);
+	
+		fs.writeFileSync(productsFilePath, productsJson);
+
+		res.redirect("/products")
+	
+	},
+
+	edit: (req, res) => {
+		const productId = parseInt(req.params.id,10);
+        let productToEdit = "";
+        for (let i=0; i<=products.length; i++) {
+            if (products[i].id === productId) {
+                productToEdit = products[i]
+                res.render("product-edit-form", {productToEdit: productToEdit})
+            } 
+        }
+	},
+
+	update: (req, res) => {
+		let idUrl = req.params.id;
+		for (let i = 0; i < products.length; i++){
+			if(products[i].id == idUrl){
+				products[i].name = req.body.name,
+				products[i].price = req.body.price,
+				products[i].discount = req.body.discount
+				products[i].category = req.body.category,
+				products[i].description = req.body.description
+				}
+			 } res.redirect("/products")
+	},
+		
+	destroy : (req, res) => { 
+			let productId = parseInt(req.params.id, 10);
+			for (let i = 0; i < products.length; i++) {
+				if ( products[i].id === productId ) {
+					products.splice(i, 1)
+				}
+			}
+			res.send(`se ha borrado el producto id ${productId}`);
+		}    
+	}
+
+module.exports = controller;
